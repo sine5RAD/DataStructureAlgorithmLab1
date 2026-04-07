18ecff517648ac21417e0ff77e58b60c2ee18cf1
sine5RAD <sinefive@outlook.com>
2026-04-07 10:04:48 +0800

    修复了眼瞎没看到height的问题 fix(AVLLTree): 现在每次更新节点都会重新计算degree

--- src/dsa/impl/AVLTree.java
+++ src/dsa/impl/AVLTree.java
@@ -5,11 +5,24 @@ import dsa.util.TreePrinter;
 
 public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
 
-   private void LeftRotate(BTPosition node){
+   private int refreshDegree(AVLPosition node) {
+      if(node == null) return 0;
+      int leftHeight = refreshDegree((AVLPosition) node.left);
+      int rightHeight = refreshDegree((AVLPosition) node.right);
+      node.degree = leftHeight - rightHeight;
+      return Math.max(leftHeight, rightHeight) + 1;
+   }
+
+   private void refreshAllDegrees() {
+      if(root == null) return;
+      refreshDegree((AVLPosition) root);
+   }
+
+   private void LeftRotate(AVLPosition node){
       assert(node.right != null);
-      var rSubtree = node.right;
-      var rSubtreeL = node.right.left;
-      var parent = node.parent;
+      AVLPosition rSubtree = (AVLPosition)node.right;
+      AVLPosition rSubtreeL = (AVLPosition)node.right.left;
+      AVLPosition parent = (AVLPosition)node.parent;
       if(parent != null){
          if(parent.left == node){
             parent.left = rSubtree;
@@ -26,11 +39,11 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
       if(rSubtreeL != null)rSubtreeL.parent = node;
    }
 
-   private void RightRotate(BTPosition node){
+   private void RightRotate(AVLPosition node){
       assert(node.left != null);
-      var lSubtree = node.left;
-      var lSubtreeR = node.left.right;
-      var parent = node.parent;
+      AVLPosition lSubtree = (AVLPosition)node.left;
+      AVLPosition lSubtreeR = (AVLPosition)node.left.right;
+      AVLPosition parent = (AVLPosition)node.parent;
       if(parent != null){
          if(parent.left == node){
             parent.left = lSubtree;
@@ -47,22 +60,59 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
       if(lSubtreeR != null)lSubtreeR.parent = node;
    }
 
+   private void LRRotate(AVLPosition node){
+      AVLPosition lSubtree = (AVLPosition)node.left;
+      AVLPosition lSubtreeR = (AVLPosition)node.left.right;
+      int lSubtreeRDegree = lSubtreeR.degree;
+
+      LeftRotate(lSubtree);
+      RightRotate(node);
+      if(lSubtreeRDegree == 0){
+         node.degree = lSubtree.degree = lSubtreeR.degree = 0;
+      }
+      else if(lSubtreeRDegree == 1){
+         lSubtree.degree = lSubtreeR.degree = 0;
+         node.degree = -1;
+      }
+      else if(lSubtreeRDegree == -1){
+         node.degree = lSubtreeR.degree = 0;
+         lSubtree.degree = 1;
+      }
+      else throw new RuntimeException();
+   }
+
+   private void RLRRotate(AVLPosition node){
+      AVLPosition rSubtree = (AVLPosition)node.right;
+      AVLPosition rSubtreeL = (AVLPosition)node.right.left;
+      int rSubtreeLDegree = rSubtreeL.degree;
+
+      RightRotate(rSubtree);
+      LeftRotate(node);
+      if(rSubtreeLDegree == 0){
+         node.degree = rSubtree.degree = rSubtreeL.degree = 0;
+      }
+      else if(rSubtreeLDegree == 1){
+         node.degree = rSubtreeL.degree = 0;
+         rSubtree.degree = -1;
+      }
+      else if(rSubtreeLDegree == -1){
+         rSubtree.degree = rSubtreeL.degree = 0;
+         node.degree = 1;
+      }
+      else throw new RuntimeException();
+
+   }
+
    @Override
    public void insert( T element ) {
       // TODO: Implement the insert(...) method.
       if(root.element == null) {expandExternal(root, element); return;}
-      BTPosition node = (BTPosition) find(root, element);
+      AVLPosition node = (AVLPosition) find(root, element);
       expandExternal(node, element);
-      restructure(node);
-   }
 
-   private int getHeight( BTPosition node)
-   {
-      if(node == null)return 0;
-      if(node.left == null && node.right == null) {return 1;}
-      if(node.left != null && node.right == null) {return 1 + getHeight(node.left);}
-      if(node.left == null) {return 1 + getHeight(node.right);}
-      return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+      refreshAllDegrees();
+      restructure(node);
+      refreshAllDegrees();
    }
 
    @Override
@@ -74,57 +124,56 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
    @Override
    public void remove( T element ) {
       // TODO: Implement the remove(...) method.
-      BTPosition node = (BTPosition) find(root, element);
-      BTPosition p = (BTPosition) node.parent;
+      AVLPosition node = (AVLPosition) find(root, element);
+      AVLPosition p = (AVLPosition) node.parent;
       if(p == null){
-         p = root.right;
+         p = (AVLPosition)root.right;
          if(p == null){
             root = root.left;
             return;
          }
          while(p.left != null || p.right != null){
-            if(p.left != null)p = p.left;
-            else p = p.right;
+            if(p.left != null)p = (AVLPosition)p.left;
+            else p = (AVLPosition)p.right;
          }
          super.remove(element);
+         refreshAllDegrees();
          restructure(p);
       }
       else{
          super.remove(element);
+         refreshAllDegrees();
          restructure(p);
 
       }
 
+      refreshAllDegrees();
+
    }
 
-   private void restructure( BTPosition n ) {
-      BTPosition node = n;
-      while(true) {
-         if(getHeight(node.left) - getHeight(node.right) >= 2) {
-            //System.out.print("L");
-            if(getHeight(node.left.left) < getHeight(node.left.right)) {
-               //System.out.print("R");
-               LeftRotate(node.left);
-               //TreePrinter.printTree(this);
+   private void restructure( AVLPosition n ) {
+      AVLPosition node = n;
+      while(node != null) {
+         if(node.degree >= 2) {
+            AVLPosition left = (AVLPosition) node.left;
+            if(left != null && left.degree < 0) {
+               LeftRotate((AVLPosition)node.left);
+               refreshAllDegrees();
             }
-            //else System.out.print("L");
             RightRotate(node);
-            //TreePrinter.printTree(this);
+            refreshAllDegrees();
          }
-         else if(getHeight(node.right) - getHeight(node.left) >= 2) {
-            //System.out.print("R");
-            if(getHeight(node.right.left) > getHeight(node.right.right)) {
-               RightRotate(node.right);
-               //System.out.print("L");
-               //TreePrinter.printTree(this);
+         else if(node.degree <= -2) {
+            AVLPosition right = (AVLPosition) node.right;
+            if(right != null && right.degree > 0) {
+               RightRotate((AVLPosition)node.right);
+               refreshAllDegrees();
             }
-            //else System.out.print("R");
             LeftRotate(node);
-            //TreePrinter.printTree(this);
+            refreshAllDegrees();
          }
-         //System.out.println(":" + node.element);
-         if(node.parent == null)break;
-         node = node.parent;
+
+         node = (AVLPosition)node.parent;
       }
    }
 
@@ -134,7 +183,7 @@ public class AVLTree<T extends Comparable<T>> extends BinarySearchTree<T> {
    }
 
    public class AVLPosition extends BTPosition {
-      int height = 0;
+      int degree = 0;// height_l - height_r
 
       public AVLPosition( T element, BTPosition parent ) {
          super( element, parent );
